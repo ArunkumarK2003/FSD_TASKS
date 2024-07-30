e9db20e6068c475947ed8cb1f77118154a2d9037
ArunkumarK2003 <arunvikram23@gmail.com>
2024-07-30 18:45:00 +0530

    Angular day 2

--- Angular/micro/src/app/app.module.ts
+++ Angular/micro/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngulaComponent } from './angula/angula.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AngulaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [
     provideClientHydration()
